e65a2d4b37215f60e76463496b21fa300cf07886
BrunaSilva25 <vendasd011@gmail.com>
2023-05-22 01:32:50 -0300

    commitmetodosoma

---
+++ src/metodojavacalc/metododecalculo.java
@@ -0,0 +1,27 @@
+package metodojavacalc;
+import java.util.Scanner;
+
+public class metododecalculo {
+	
+//mandar github
+		public static void main(String[] args) {
+		System.out.println("digite numero pra a:");
+		Scanner entrada = new Scanner(System.in);
+		int a = entrada.nextInt();
+		
+		System.out.println("digite numero pra a:");
+		Scanner entrada2 = new Scanner(System.in);
+		int b = entrada2.nextInt();
+		
+		int total = metododecalculo.somar(a, b);
+	    System.out.println(total);
+		}
+	  //metodo somar
+		public static int somar(int a, int b) {
+			return(a+b);
+		}
+		
+		
+	}
+
+
